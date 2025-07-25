c2023389a33ae74b8d60acb4ab19ab39c5ef4136
Xolelwa <xolelwacekiso@gmail.com>
2025-07-25 10:38:54 +0200

    added

--- modulas.js
+++ modulas.js
@@ -3,7 +3,7 @@ let num1, num2;
 let modulus= function (num1,num2){
     num1=100;
     num2=5;
-    let answer = num2%num1;
+    let answer = num1%num2;
     console.log(answer)
 }
 modulus(num1,num2)
